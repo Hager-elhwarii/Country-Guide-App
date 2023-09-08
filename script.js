115656cdc2483547e82229fcaa8a07e76b161d05
hager-dot <hagerelhwarii6633@gmail.com>
2023-09-08 21:49:50 +0300

    :sparkles: resytle the country data interface

--- script.js
+++ script.js
@@ -21,22 +21,26 @@ searchBtn.addEventListener("click", () => {
       console.log(data[0].population);
       console.log(Object.values(data[0].currencies)[0].name);
       console.log(Object.values(data[0].languages)[0]);
-      result.innerHTML = `<div class="country-data">
-                     <img class='country-flag' src=${data[0].flags.png} alt='country flag' />
-                    <h3>Country Name: ${data[0].name.common}</h3>
-                    <h3>Capital: ${data[0].capital[0]}</h3>
-                    <h3>Continets: ${data[0].continents[0]}</h3>
-                    <h3>Population: ${data[0].population}</h3>
-                    <h3>Currency: ${Object.values(data[0].currencies)[0].name}</h3>
-                    <h3>Language: ${Object.values(data[0].languages)[0]}</h3>
-                    </div>`;
+      result.innerHTML = `
+
+      <div class="country-data">
+      <img class='country-flag' src=${data[0].flags.png} alt='country flag' />
+     <h3 class='country-name'>${data[0].name.common}</h3>
+     <div class='data-wrapper'>
+     <h4>Capital: <span>${data[0].capital[0]}</span></h4>
+     <h4>Continent: <span>${data[0].continents[0]}</span></h4>
+     <h4>Population: <span>${data[0].population}</span> </h4>
+     <h4>Currency: <span>${Object.values(data[0].currencies)[0].name}</span></h4>
+     <h4>Language: <span>${Object.values(data[0].languages)[0]}</span></h4>
+     </div>
+     </div>
+      `;
     })
     .catch(() => {
-      if(countryName == ''){
-        result.innerHTML = `<h3>Your input shouldn't be empty.</h3>`
-      }
-      else {
-        result.innerHTML = `<h3>Not a valid Country name.</h3>`
+      if (countryName == "") {
+        result.innerHTML = `<h3 class='empty-input'>The input field cannot be empty.</h3>`;
+      } else {
+        result.innerHTML = `<h3 class='unvalid-input'>Please enter a valid Country name.</h3>`;
       }
     });
 });
